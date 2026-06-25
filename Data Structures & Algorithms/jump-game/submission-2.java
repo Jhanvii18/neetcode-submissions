c402e221009fd3b739bbd9daf73f93c7cd69a2bf
Jhanvi k <jhanvi180506@gmail.com>
2026-06-25 18:36:36 +0530

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.java
@@ -0,0 +1,16 @@
+//using a flag like soln watch nikhil lohia 
+class Solution 
+{
+    public boolean canJump(int[] nums) 
+    {
+       int farthest = nums.length-1;
+       for(int i= nums.length-2;i>=0;i--)
+       {
+          if(i+nums[i]>=farthest)
+          {
+            farthest = i;
+          }
+       } 
+       return farthest==0;
+    }
+}
