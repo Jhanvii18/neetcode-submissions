0e4a1f7c7b9117be76104e2613fc7d8363f814c8
Jhanvi k <jhanvi180506@gmail.com>
2026-07-05 21:45:57 +0530

    Add: maximum-product-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-3.java
@@ -0,0 +1,29 @@
+class Solution 
+{
+    public int maxProduct(int[] nums) 
+    {
+        int right = 1;
+        int left = 1;
+        int n = nums.length;
+
+        if(nums.length<=1)
+        return nums[n-1];
+
+        int max=nums[0];
+        for(int i=0;i<nums.length;i++)
+        {
+            if(left==0)
+            left=1;
+
+            if(right==0)
+            right=1;
+
+            right=right*nums[n-i-1];
+
+            left=left*nums[i];
+
+            max=Math.max(max,Math.max(left,right));
+        }
+        return max;
+    }
+}
