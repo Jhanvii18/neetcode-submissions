36b7c27dc9ccca89b3948fee8fb5428e71c57938
Jhanvi k <jhanvi180506@gmail.com>
2026-07-08 15:54:06 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.java
@@ -0,0 +1,17 @@
+class Solution 
+{
+    public int reverseBits(int n) 
+    {
+        int ans =0;
+        int mask =1;
+        for(int i=0;i<32;i++)
+        {
+            if((n&mask)!=0)
+            {
+                ans+=1<<31-i;//31 - i not 32
+            }
+            mask<<=1;
+        }
+        return ans;
+    }
+}
